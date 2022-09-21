1c2ea4a1579995fbab02510e7aac55f51ae40f67
unknown <y.shirogane.sys22@morijyobi.ac.jp>
2022-09-21 12:22:14 +0900

    git pra2.javaでHello Wolrdを 100階だせるようにした

--- pra2.java
+++ pra2.java
@@ -1,5 +1,10 @@
 class pra2 {
 	public static void main(String[] args){
-		System.out.println("Hello World")
+		//System.out.println("Hello World");
+		String a ="Hello World";
+		for(int i = 1;i <= 100;i ++){
+			System.out.println(a);
+		}
 
-
+}
+}
